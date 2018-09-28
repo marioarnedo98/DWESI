987a168748a3ba076d8cfbfab2634934c2cc7bca
Mario <marioarnedo1@gmail.com>
2018-09-28 12:16:39 +0200

    2809

---
+++ Tema1_start/WP/20.php
@@ -0,0 +1,21 @@
+<?php
+$cities= array(
+    array("Barcelona",1602386),
+    array("Sevilla",696676),
+    array("Malaga",566913),
+    array("Palma de Mallorca",399093),
+    array("Bilbao",346574),
+    array("Madrid",3151689),
+    array("Valencia",787266),
+    array("Zaragoza",666058),
+    array("Murcia",439712),
+    array("Las Palmas de Gran Canaria",382283)
+);
+echo "<table>";
+foreach ($cities as $key=>$values) {
+    echo ("<tr>");
+        echo ("<td>".$values[0]."</td>");
+        echo ("<td>".$values[1]."</td>");
+    echo("</tr>");
+}
+echo("</table>");
