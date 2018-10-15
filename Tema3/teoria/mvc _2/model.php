dfd42fc1d15683db5132bee65df625003c133893
Mario <marioarnedo1@gmail.com>
2018-10-15 12:51:43 +0200

    1510

--- Tema3/teoria/mvc _2/model.php
+++ Tema3/teoria/mvc _2/model.php
@@ -20,8 +20,8 @@ function get_post_by_id($id){
     $link=open_database_connection();
     $query= 'SELECT created_at, title, body FROM post WHERE id=:id';
     $statement = $link->prepare($query);
-    $statement=bindValue(':id', $id, PDO::PARAM_INT);
-    $statement->execute;
+    $statement->bindValue(':id', $id, PDO::PARAM_INT);
+    $statement->execute();
     $row= $statement->fetch(PDO::FETCH_ASSOC);
     close_database_connection($link);
     return $row;
