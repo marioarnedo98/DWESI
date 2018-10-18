d267fca43681172f6c0f019777e5045b56af6cc9
Mario <marioarnedo1@gmail.com>
2018-10-18 09:31:45 +0200

    1810

---
+++ Tema3/bookstore/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+//define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "bookstore");
+
+//Define URL
+define("ROOT_PATH", "/Tema3/bookstore");
+define("ROOT_URL", "http://dwesi/Tema3/bookstore/");
---
+++ Tema3/bookstore/index.php
@@ -0,0 +1,10 @@
+<?php
+// Start Session
+// session_start();
+
+//composer autoloader
+// require_once 'vendor/autoload.php';
+require_once 'config/config.php';
+// require_once 'config/database.php';
+
+$app= new App;
