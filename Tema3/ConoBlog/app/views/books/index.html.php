ecfc95f7720821b0d6d3a12e63f26e2d4f93401f
Mario <marioarnedo1@gmail.com>
2018-11-12 12:48:30 +0100

    1211

--- Tema3/ConoBlog/app/views/books/index.html.php
+++ Tema3/ConoBlog/app/views/books/index.html.php
@@ -1,49 +1,6 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-    <meta charset="utf-8">
-    <link rel="shortcut icon" href="assets/img/favicon.ico">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MacGregor</title>
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/fonts/font-awesome.min.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/fonts/ionicons.min.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Article-List.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Footer-Clean.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Login-Form-Dark.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Navigation-Clean.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/styles.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Team-Boxed.css">
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/normalize.css" />
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/demo.css" />
-    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/component.css" />
-    
-</head>
-
-<body style="
-background-color: #FFF;
-">
-    <div>
-        <nav class="navbar navbar-light navbar-expand-md navigation-clean">
-            <div class="container"><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                <div class="collapse navbar-collapse"
-                    id="navcol-1">
-                <nav class="menu menu--sebastian">
-					<ul class="menu__list">
-                        <li class="menu__item"><a href="index.html" class="menu__link oculta">Inicio</a></li>
-						<li class="menu__item"><a href="calendario.html" class="menu__link">Calendario</a></li>
-                        <li class="menu__item"><a href="galeria.html" class="menu__link">Galeria</a></li>
-                        <li class="menu__item icono"><a class="navbar-brand" href="/cono"><img src="<?=ROOT_URL?>/public/img/logoconor.png" class="logo" alt="cono mcgregor" ></a></li>
-						<li class="menu__item"><a href="sobre_mi.html" class="menu__link">Sobre Mi</a></li>
-						<li class="menu__item"><a href="contacto.html" class="menu__link">Contacto</a></li>
-					</ul>
-				</nav>
-                </div>
-            </div>
-        </nav>
-    </div>
-         
+<?php
+ob_start();
+?>      
     <div class="carousel slide" data-ride="carousel" id="carousel-1">
         <div class="carousel-inner" role="listbox">
             <div class="carousel-item active">
@@ -93,41 +50,5 @@ background-color: #FFF;
             </div>
         </div>
     </div>
-    <div class="footer-clean">
-        <footer>
-            <div class="container">
-                <div class="row justify-content-center">
-                    <div class="col-sm-4 col-md-3 item">
-                        <h3>Menu</h3>
-                        <ul>
-                            <li><a href="index.html">Inicio</a></li>
-                            <li><a href="calendario.html">Calendario</a></li>
-                            <li><a href="galeria.html">Galeria</a></li>
-                            <li><a href="sobre_mi.html">Info</a></li>
-                            <li><a href="contacto.html">Contacto</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-sm-4 col-md-3 item">
-                        <h3>Contacto</h3>
-                        <ul>
-                            <li><a href="mailto:mcgregor@ufcfighter.com">Email: mcgregor@ufcfighter.com</a></li>
-                        </ul>
-                        <ul>
-                            <li><a>Telefono: (1) 866 415 1336</a></li>
-                        </ul>
-                    </div>
-                    <div class="col-lg-3 item social"><a href="https://www.facebook.com/thenotoriousmma/"><i class="icon ion-social-facebook"></i></a><a href="https://twitter.com/TheNotoriousMMA"><i class="icon ion-social-twitter"></i></a><a href="https://www.instagram.com/thenotoriousmma"><i class="icon ion-social-instagram"></i></a>
-                        <p class="copyright">Hecho con <i>♥</i> por Mariquitos</p>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    </div>
-    <script src="assets/js/jquery.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/js/classie.js"></script>
-    <script src="assets/js/clipboard.min.js"></script>
-  
-</body>
-
-</html>
+  <?php $content= ob_get_clean();
+include 'app/views/layout.html.php';
--- Tema3/ConoBlog/app/views/layout.html.php
+++ Tema3/ConoBlog/app/views/layout.html.php
@@ -5,40 +5,86 @@ $pag_cortada= explode("/", $pag_actual);
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="<?=ROOT_URL?>/public/css/bootstrap.min.css">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-<link rel="stylesheet" href="<?=ROOT_URL?>/public/css/override.css">
-<link rel="stylesheet" href="<?=ROOT_URL?>/public/css/login_user.css">
-<link rel="icon" type="image/png" href="<?=ROOT_URL?>/public/images/favicon.png">
-
+    <meta charset="utf-8">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MacGregor</title>
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Article-List.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Footer-Clean.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Navigation-Clean.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/styles.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/Team-Boxed.css">
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/normalize.css" />
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/demo.css" />
+    <link rel="stylesheet" href="<?=ROOT_URL?>/public/css/component.css" />
+    
 </head>
+
+<body style="
+background-color: #FFF;
+">
+    <div>
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean">
+            <div class="container"><button class="navbar-toggler" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                <nav class="menu menu--sebastian">
+					<ul class="menu__list">
+                        <li class="menu__item"><a href="index.html" class="menu__link oculta">Inicio</a></li>
+						<li class="menu__item"><a href="calendario.html" class="menu__link">Calendario</a></li>
+                        <li class="menu__item"><a href="galeria.html" class="menu__link">Galeria</a></li>
+                        <li class="menu__item icono"><a class="navbar-brand" href="/cono"><img src="<?=ROOT_URL?>/public/img/logoconor.png" class="logo" alt="cono mcgregor" ></a></li>
+						<li class="menu__item"><a href="sobre_mi.html" class="menu__link">Sobre Mi</a></li>
+						<li class="menu__item"><a href="contacto.html" class="menu__link">Contacto</a></li>
+					</ul>
+				</nav>
+                </div>
+            </div>
+        </nav>
+    </div>
 <body class="<?php echo($pag_cortada[sizeof($pag_cortada)-1].$pag_cortada[sizeof($pag_cortada)-2])?>"> 
-    <div class="topnav">
-    <a class="active home" href="<?=ROOT_URL?>">Home</a>
-
-    <div class="topnav-right">
-    <?php if(isset($_SESSION['is_logged_in'])):?>
-    <!-- <p>Bienvenido  </p> -->
-    <!-- <a href="<?= ROOT_URL?>users/logout"">Logout</a> -->
-
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Bienvenido <?= print_r($_SESSION['user_data']['name'],true); ?><span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a class="logout" href="<?= ROOT_URL?>users/logout"">Logout</a></li>
-            <!-- <li><p>Last Login: <?php echo $_COOKIE["HeCookie"]; ?></p></li> -->
-    <?php else:?>
-    <a href="<?= ROOT_URL?>users/login">Login</a>
-      <a href="<?= ROOT_URL?>users/register">Register</a>
-    <?php endif;?>
-    </div>
-    </div>
-<div class="container">
+<div class="main">
 <?=print_r (Messages::display(),true)?>
 <?=$content?>
 
 </div>
-
+<div class="footer-clean">
+        <footer>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Menu</h3>
+                        <ul>
+                            <li><a href="index.html">Inicio</a></li>
+                            <li><a href="calendario.html">Calendario</a></li>
+                            <li><a href="galeria.html">Galeria</a></li>
+                            <li><a href="sobre_mi.html">Info</a></li>
+                            <li><a href="contacto.html">Contacto</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Contacto</h3>
+                        <ul>
+                            <li><a href="mailto:mcgregor@ufcfighter.com">Email: mcgregor@ufcfighter.com</a></li>
+                        </ul>
+                        <ul>
+                            <li><a>Telefono: (1) 866 415 1336</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 item social"><a href="https://www.facebook.com/thenotoriousmma/"><i class="icon ion-social-facebook"></i></a><a href="https://twitter.com/TheNotoriousMMA"><i class="icon ion-social-twitter"></i></a><a href="https://www.instagram.com/thenotoriousmma"><i class="icon ion-social-instagram"></i></a>
+                        <p class="copyright">Hecho con <i>♥</i> por Mariquitos</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+<script src="<?=ROOT_URL?>/public/js/jquery.min.js"></script>
+    <script src="<?=ROOT_URL?>/public/bootstrap/js/bootstrap.min.js"></script>
+    <script src="<?=ROOT_URL?>/public/js/classie.js"></script>
+    <script src="<?=ROOT_URL?>/public/js/clipboard.min.js"></script>
 </html>
